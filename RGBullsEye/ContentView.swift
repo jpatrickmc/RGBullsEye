d6e69f424f1bd6e803d68b51436dca1d0adda363
Patrick McDaniel <dfwjpm@outlook.com>
2025-10-10 12:47:52 -0500

    add text and slider

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -34,8 +34,23 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+      
+      VStack {
+          Color(red: 0.5, green: 0.5, blue: 0.5)
+          Text("R: ??? G: ??? B: ???")
+              .padding()
+          
+          Color(red: 0.5, green: 0.5, blue: 0.5)
+          Text("R: 204 G: 76 B: 178")
+              .padding()
+          
+         Slider(value: .constant(0.5))
+              
+          Button("Hit Me!") {
+              /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+          }
+      }
+      
   }
 }
 
